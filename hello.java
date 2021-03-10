131ea5beae164c838d57a766bed6bf0c497a2684
Cornzi <63601122+Cornzi@users.noreply.github.com>
2021-03-10 10:49:46 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 import java.awt.Graphics;
 import java.applet.Applet;
-public class hell
+public class hello extends Applet
 {
   public  void paint(Graphics g){
   g.drawString("hello world",150,45);
